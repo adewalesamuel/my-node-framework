0ef253e334c39dce219f58508a227e3f6131ae14
unknown <samuel.adewale@epistrophe.ci>
2021-09-16 18:49:44 +0000

    Bugfix

--- controllers/root/index.js
+++ controllers/root/index.js
@@ -2,6 +2,7 @@ const  fs = require('fs');
 const VARS = require('../../vars');
 
 const index = (request, response) => {
+    response.setHeader('Content-type', 'text/plain');
     response.write("Hello world. this is my node framework");
     response.end();
 }
